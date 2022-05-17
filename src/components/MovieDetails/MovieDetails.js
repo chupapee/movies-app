ba7b98c555f718d03b1932301e8a678cca562c90
ComeAll <kamolkhamidov200002@gmail.com>
2022-05-17 22:28:31 +0300

    added styles to movieDetails on hover

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -6,9 +6,14 @@ console.log(info);
   return (
     <div className='movieWrapper'>
       <div className='moviePosterWrapper'>
-        <img className='moviePoster' src={info.Poster} />
-        <span className='movieTitle'>{info.Title}</span>
-        <span className='movieYear'>{info.Year}</span>
+        <div className='moviePosterWrapper'>
+          <img className='moviePoster' src={info.Poster} />
+          <div className='movieOverlay' />
+        </div>
+        <div className='movieDesctiption'>
+          <span className='movieTitle'>{info.Title}</span>
+          <span className='movieYear'>{info.Year}</span>
+        </div>
       </div>
     </div>
   )
