40e199e7802e9b8c3d96f8c586aa8eba52c935a1
ComeAll <kamolkhamidov200002@gmail.com>
2022-07-02 16:47:59 +0300

    changed http to https

--- src/dal/searchMovieAPI.js
+++ src/dal/searchMovieAPI.js
@@ -4,11 +4,11 @@ const API_KEY = '4687b36d'
 
 export const searchApi = {
   findMovie(movieTitle){
-    return axios.get(`http://www.omdbapi.com/?s=${movieTitle}&apikey=${API_KEY}&page=1`)
+    return axios.get(`https://www.omdbapi.com/?s=${movieTitle}&apikey=${API_KEY}&page=1`)
     .then(response => { return response.data })
   },
   fetchInfo(movieTitle){
-    return axios.get(`http://www.omdbapi.com/?t=${movieTitle}&plot=full&apikey=${API_KEY}`)
+    return axios.get(`https://www.omdbapi.com/?t=${movieTitle}&plot=full&apikey=${API_KEY}`)
     .then(response => { return response.data })
   }
 }
