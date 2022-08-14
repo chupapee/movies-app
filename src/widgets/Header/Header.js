e5b19baa2c8c2453fc8e17e85289cc4950c10ec2
ComeAll <kamolkhamidov200002@gmail.com>
2022-08-15 01:10:03 +0300

    adding nab burger

--- src/widgets/Header/Header.js
+++ src/widgets/Header/Header.js
@@ -3,12 +3,25 @@ import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import s from "./header.module.css";
 import { getNavlist } from "./nav";
+import MenuIcon from '@mui/icons-material/Menu';
+import { useRef } from "react";
+import { useState } from "react";
 
 export function Header() {
-  const login =
-    useSelector((state) => state.auth.login) || localStorage.getItem("login");
+  const login = useSelector((state) => state.auth.login) || localStorage.getItem("login");
 
   const navList = getNavlist(login);
+
+  const [isOpened, setIsOpened] = useState(false)
+  const navRef = useRef(null)
+  function openNav() {
+    setIsOpened(!isOpened)
+    if(isOpened){
+      navRef.current.className = `${s.navList} ${s.showNav}`
+    } else {
+      navRef.current.className = s.navList
+    }
+  }
 
   return (
     <div className={s.header}>
@@ -17,8 +30,9 @@ export function Header() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"
         alt="imdbIcon"
       />
-      <nav className={s.nav}>
-        <ul className={s.navList}>
+        <div onClick={openNav} className={s.burger}><MenuIcon /></div>
+      <nav  className={s.nav}>
+        <ul ref={navRef} className={s.navList}>
           {navList.map(({ value, path }) => (
             <li key={value}>
               <NavLink className={s.navItem} to={path}>
