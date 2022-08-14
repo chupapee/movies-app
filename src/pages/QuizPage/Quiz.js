1bdcc43ca8948f032de2d14726cf17feb8ff4376
ComeAll <kamolkhamidov200002@gmail.com>
2022-08-14 17:44:53 +0300

    adding logic to quiz

--- src/pages/QuizPage/Quiz.js
+++ src/pages/QuizPage/Quiz.js
@@ -4,9 +4,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { getQuizList, nextQuiz } from "../../redux/slices.js/quizSlice";
 import { BackBtn } from "../../shared/buttons/components/BackBtn";
 import s from "./quiz.module.css";
+import { Preloader } from '../../widgets/Preloader/Preloader'
 
 export const Quiz = () => {
   const quiz = useSelector((state) => state.quiz.currentQuiz);
+  const isLoading = useSelector((state) => state.quiz.isLoading);
   const dispatch = useDispatch();
 
   const getQuiz = () => {
@@ -16,53 +18,68 @@ export const Quiz = () => {
     getQuiz();
   }, []);
 
-  const [countQuiz, setCountQuiz] = useState(0);
+  const [countQuiz, setCountQuiz] = useState(1);
+
+  const [over, setOver] = useState(false);
   const getNextQuiz = () => {
+    if (attemptCount === 1) {
+      SetAttemptCount(0);
+      setOver(true);
+      return;
+    }
     setCountQuiz(countQuiz + 1);
     dispatch(nextQuiz(countQuiz));
     setTimeout(() => setAnimation(false), 500);
   };
 
-  const [isGuessed, setIsGuessed] = useState(false);
+  const [guessedCount, SetGuessedCount] = useState(0);
+  const [attemptCount, SetAttemptCount] = useState(5);
   const [animation, setAnimation] = useState(false);
+
   const checkAnswer = (answer) => {
-    setIsGuessed(answer === quiz.answer);
-    setAnimation(true);
+    if (answer === quiz.answer) SetGuessedCount(guessedCount + 1);
+    else SetAttemptCount(attemptCount - 1);
+    setTimeout(() => setAnimation(true), 500);
     setTimeout(getNextQuiz, 1000);
   };
-
-  const tick = () => {
-    if(timer === 0) return
-    setTimer(timer - 1)
-  }
-
-  useEffect(() => {
-    const timerID = setInterval(() => tick(), 1000);
-    return () => clearInterval(timerID);
-  });
-
-  const [timer, setTimer] = useState(5);
 
   return (
     <>
       <BackBtn />
       <div className={s.quizWrap}>
-        <div className={s.quizTimer}>
-          <span>{timer}</span>
-        </div>
         <div className={s.quizStatus}>
-          {isGuessed ? <p>win</p> : <p>failed</p>}
+          {!over && (
+            <>
+              <p>Question: {countQuiz} / 25</p>
+              <p>Attempts: {attemptCount}</p>
+            </>
+          )}
         </div>
-        <div className={`${s.quizQuestion} ${animation && s.animation}`}>
-          <p>{quiz.question}</p>
-        </div>
-        <div className={`${s.quizOptions} ${animation && s.animation}`}>
-          {quiz.options &&
-            quiz.options.map((value) => (
-              <button className={s.quizBtn} onClick={() => checkAnswer(value)} key={value}>
-                {value}
-              </button>
-            ))}
+        <div className={s.quizContent}>
+          {over ? (
+            <div className={s.quizOver}>
+              <p>Game over!</p>
+              <p>Guessed: {guessedCount}</p>
+            </div>
+          ) : (
+            <>
+              <div className={`${s.quizQuestion} ${animation && s.animation}`}>
+                {isLoading ? <Preloader /> : <p>{quiz.question}</p>}
+              </div>
+              <div className={s.quizOptions}>
+                {quiz.options &&
+                  quiz.options.map((value) => (
+                    <button
+                      className={`${s.quizBtn} ${animation && s.animation}`}
+                      onClick={() => checkAnswer(value)}
+                      key={value}
+                    >
+                      {value}
+                    </button>
+                  ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
