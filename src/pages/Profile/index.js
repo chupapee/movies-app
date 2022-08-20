2af19148b38d47c8150f1712c05ffc832d2b8e3e
ComeAll <kamolkhamidov200002@gmail.com>
2022-08-20 19:19:55 +0300

    adding profile page styles

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,16 +2,17 @@ import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setImg, setLogin } from "../../redux/slices.js/authSlice";
 import s from "./style.module.css";
+import { BackBtn } from '../../shared/buttons/components/BackBtn'
 
 export const Profile = () => {
   const login = useSelector((state) => state.auth.login);
   const dispatch = useDispatch();
   const loginRef = useRef(null);
-  const defaultImg = useSelector(state => state.auth.img)
+  const defaultImg = useSelector((state) => state.auth.img);
 
   const [disabled, setDisabled] = useState(true);
   function handleChanging() {
-    setDisabled(false);
+    setDisabled(!disabled);
   }
 
   useEffect(() => {
@@ -21,7 +22,7 @@ export const Profile = () => {
   function handleSaving() {
     dispatch(setLogin(loginValue));
     setDisabled(true);
-    dispatch(setImg(image))
+    dispatch(setImg(image));
   }
 
   const [loginValue, setLoginValue] = useState(login);
@@ -31,7 +32,7 @@ export const Profile = () => {
 
   function handleKeyDown(e) {
     if (e.code === "Enter") {
-      setDisabled(true)
+      setDisabled(true);
     }
   }
 
@@ -49,28 +50,47 @@ export const Profile = () => {
   return (
     <>
       <div className={s.wrap}>
-        <div className={s.imgWrap}>
-          <img src={image} />
-          <input
-            name="profileImg"
-            onChange={handleFileChange}
-            type={"file"}
-          />
+        <BackBtn />
+        <div className={s.profileWrap}>
+          <div className={s.contentWrap}>
+            <div className={s.imgWrap}>
+              <img src={image} alt="profile_icon" />
+              <label className={s.btn}>
+                <input
+                  style={{ display: "none" }}
+                  id="imgInput"
+                  onChange={handleFileChange}
+                  type={"file"}
+                />
+                select image
+              </label>
+            </div>
+            <div className={s.loginForm}>
+              <label className={s.loginInp}>
+                Login
+                <input
+                  className={s.loginInp}
+                  id="loginInp"
+                  onKeyDown={handleKeyDown}
+                  ref={loginRef}
+                  required
+                  onChange={(e) => changeLogin(e.target.value)}
+                  type="text"
+                  value={loginValue}
+                  disabled={disabled}
+                />
+              </label>
+              <div className={s.btnsWrap}>
+                <button className={s.btn} onClick={handleChanging}>
+                  Edit
+                </button>
+                <button className={s.btn} onClick={handleSaving}>
+                  save changes
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={s.loginForm}>
-          <input
-            onKeyDown={handleKeyDown}
-            ref={loginRef}
-            className={s.loginInp}
-            required
-            onChange={(e) => changeLogin(e.target.value)}
-            type="text"
-            value={loginValue}
-            disabled={disabled}
-          />
-          <button onClick={handleChanging}>change</button>
-        </div>
-        <button onClick={handleSaving}>save</button>
       </div>
     </>
   );
