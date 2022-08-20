bf096ed4e162fc0b65894f91a84a5ae3f817d40e
ComeAll <kamolkhamidov200002@gmail.com>
2022-08-20 23:06:32 +0300

    removing invalid btn

--- src/service/searchMovieAPI.js
+++ src/service/searchMovieAPI.js
@@ -5,7 +5,7 @@ const API_KEY = '4687b36d'
 export const searchApi = {
   findMovie(movieTitle, pageNum){
     return axios.get(`https://www.omdbapi.com/?s=${movieTitle}&apikey=${API_KEY}&page=${pageNum}`)
-      .then(response => { return response.data })
+      .then(response => { return response.data; })
   },
   fetchInfo(movieTitle){
     return axios.get(`https://www.omdbapi.com/?t=${movieTitle}&plot=full&apikey=${API_KEY}`)
--- src/widgets/Movies/MovieInfo/MovieInfo.js
+++ src/widgets/Movies/MovieInfo/MovieInfo.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import s from './movieInfo.module.css'
 
@@ -15,9 +14,6 @@ export function MovieInfo({info}) {
             <div className={s.movieCalendar}>
             <CalendarMonthIcon/>
             <span className={s.movieYear}>{info.Year}</span>
-            </div>
-            <div className={s.movieLikeWrap}>
-              <FavoriteIcon className={s.movieLike} />
             </div>
           </div>
     </div>
